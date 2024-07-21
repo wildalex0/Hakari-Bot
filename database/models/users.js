d1d58e8b0b01f0f759d8f8ea08ac514887946b1b
wildalex0 <129412733+wildalex0@users.noreply.github.com>
2024-07-21 17:50:47 +0200

    started DBs, added some base folders and files

---
+++ database/models/users.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('users', {
+        user_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.TEXT
+        },
+        current_balance: {
+            type: DataTypes.INTEGER
+        },
+        prestige_multiplier: {
+            type: DataTypes.REAL
+        },
+        last_active: {
+            //Technically type timestamp but due to SQLITE being used it is TEXT
+            type: DataTypes.TEXT
+        }
+    })
+}
